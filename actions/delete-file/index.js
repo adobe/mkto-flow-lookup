66423e76f6e83ce022529fdde5577e469523a784
kelkingt <kelkingt@adobe.com>
2021-05-24 15:04:42 -0700

    Initial Commit: Simple File Manager, No Authentication Yet

---
+++ actions/delete-file/index.js
@@ -0,0 +1,64 @@
+const filesLib = require('@adobe/aio-lib-files');
+
+
+/**
+ * Derived from sample action showcasing how to access an external API
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+
+const fetch = require('node-fetch')
+const { Core, Target } = require('@adobe/aio-sdk')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main(params) {
+  const files = await filesLib.init()
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('Calling the main action')
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.debug(stringParameters(params))
+
+    // check for missing request input parameters and headers
+    const requiredParams = ['target'];
+    const requiredHeaders = [
+      //'Authorization', 
+    ];
+    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    if (errorMessage) {
+      // return and log client errors
+      return errorResponse(400, errorMessage, logger)
+    }
+
+    // extract the user Bearer token from the Authorization header
+    const token = getBearerToken(params)
+
+    var response = {};
+    try {
+      await files.delete(params.target);
+      response["statusCode"] = 200;
+    } catch (error) {
+      logger.info("caught error:")
+      return errorResponse(400, error, logger);
+    }
+    return response;
+  } catch (error) {
+    // log any server errors
+    logger.error(error)
+    // return with 500
+    return errorResponse(500, 'server error', logger)
+  }
+}
+
+exports.main = main
---
+++ actions/publish-events/index.js
@@ -0,0 +1,86 @@
+/*
+* <license header>
+*/
+
+/**
+ * This is a sample action showcasing how to create a cloud event and publish to I/O Events
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+
+const { Core, Events } = require('@adobe/aio-sdk')
+const uuid = require('uuid')
+const cloudEventV1 = require('cloudevents-sdk/v1')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main (params) {
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('Calling the main action')
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.debug(stringParameters(params))
+
+    // check for missing request input parameters and headers
+    const requiredParams = ['apiKey', 'providerId', 'eventCode', 'payload']
+    const requiredHeaders = ['Authorization', 'x-gw-ims-org-id']
+    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    if (errorMessage) {
+      // return and log client errors
+      return errorResponse(400, errorMessage, logger)
+    }
+
+    // extract the user Bearer token from the Authorization header
+    const token = getBearerToken(params)
+
+    
+    // initialize the client
+    const orgId = params.__ow_headers['x-gw-ims-org-id']
+    const eventsClient = await Events.init(orgId, params.apiKey, token)
+
+    // Create cloud event for the given payload
+    const cloudEvent = createCloudEvent(params.providerId, params.eventCode, params.payload)
+
+    // Publish to I/O Events
+    const published = await eventsClient.publishEvent(cloudEvent)
+    let statusCode = 200
+    if (published === 'OK') {
+      logger.info('Published successfully to I/O Events')
+    } else if (published === undefined) {
+      logger.info('Published to I/O Events but there were not interested registrations')
+      statusCode = 204
+    }
+    const response = {
+      statusCode: statusCode,
+    }
+
+    // log the response status code
+    logger.info(`${response.statusCode}: successful request`)
+    return response
+  } catch (error) {
+    // log any server errors
+    logger.error(error)
+    // return with 500
+    return errorResponse(500, 'server error', logger)
+  }
+}
+
+function createCloudEvent(providerId, eventCode, payload) {
+  let cloudevent = cloudEventV1.event()
+    .data(payload)
+    .source('urn:uuid:' + providerId)
+    .type(eventCode)
+    .id(uuid.v4())
+  return cloudevent.format()
+}
+exports.main = main
