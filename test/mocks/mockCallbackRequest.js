37ff5daf73489cac10df4e39744fbafe080e910f
kelkingt <kelkingt@adobe.com>
2021-07-22 16:58:16 -0700

    mockCallbackResponse tests passing.  executeCallback still has issues

---
+++ test/mocks/mockCallbackRequest.js
@@ -0,0 +1,26 @@
+const { actionPrefix } = require('../../e2e/constants')
+const mockCallbackUrl = `${actionPrefix}/mockCallbackResponse`;
+
+const mockSingleLead = {
+        "munchkinId": "AAA-999-ZZZ",
+        "token": "0550e558-530a-40aa-83a8-c02c7a6831cb",
+        "time": new Date().toISOString(),
+        "objectData": [
+            {
+                "leadData": {
+                    "id": 1000,
+                    "country-code-2": "ZW"
+                },
+                "activityData": {
+                    "foo": "bar",
+                    "baz": "foo",
+                    "bar": "baz",
+                    "int": 50
+                }
+            }
+        ]
+};
+
+module.exports = {
+    mockSingleLead
+}
